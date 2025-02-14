96b4507a355d0edf58b00430ab2c03a11ebee660
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2025-02-13 23:09:05 -0500

    added more graphs

--- test/quantgbm.jl
+++ test/quantgbm.jl
@@ -1,6 +1,7 @@
 using Test
 using Random
 using UnicodePlots
+using Plots
 using Statistics
 include("../quant.jl")
 include("../data.jl")
@@ -36,16 +37,16 @@ include("../data.jl")
 
 
     LOOK_BACK_PERIOD = 100
-    NUM_EPISODES = 100
+    NUM_EPISODES = 300
     
     for i in 1:NUM_EPISODES
         if (i % 10 == 0)
             println("Episode: $i")
         end
+
         current_capital = 1000.0
         total_reward = 0;
         max_reward = -Inf  # Initialize max_reward for the episode
-      
         d = 0.0
         episode_length = 0
     
@@ -59,20 +60,21 @@ include("../data.jl")
             # Normalize state
             s = vcat(price_vscores[t - LOOK_BACK_PERIOD + 1:t], [(log10(current_capital) - mean_capital) / std_capital])
         
-            # Generate action and scale reward
-            ε = randn() * .25
+            # Generate action 
+            ε = (i <=100 ) ? randn() * .25 : 0.0
             a = clamp(ε + quant.π_(s)[1], -1, 1)
             capital_allocation = current_capital * min(abs(a), .5)
             current_capital -= capital_allocation
 
+            # Calculate reward
             percent_change = price_data[t + 1]
             r = (a > 0.0 ? 1 : -1) * capital_allocation * (percent_change / 100.0)
             max_reward = max(max_reward, r)  # Update max_reward
             scaled_r = r / max_reward  # Scale reward
 
             # println("CAPITAL BEFORE: $(current_capital+capital_allocation) ACTION: $a $(a < 0 ? "SHORT" : "LONG"): $capital_allocation PRICE CHANGE: $percent_change REWARD: $scaled_r")
-        
             current_capital += scaled_r * max_reward + capital_allocation
+
 
             push!(capitals, current_capital)
             if length(capitals) > 100
@@ -90,14 +92,21 @@ include("../data.jl")
                 d = 1.0
             end
 
-            # Train 
+
             add_experience!(quant, s, a, scaled_r, s′, d)
             train!(quant, 0.0001, 0.0001, 64)
         end
         
+        if i % 100 == 0 || i == 1
+            Plots.histogram(capitals[end - episode_length + 2:end], title="Episode $i Capital Distribution", xlabel="Capital", ylabel="Frequency")
+            Plots.savefig("plots/capital_distribution/episode_$(i).png")
+        end
+
         push!(total_rewards, total_reward)
     end 
     
+    Plots.histogram(capitals, title="Full Capital Distribution", xlabel="Capital", ylabel="Frequency")
+    Plots.savefig("plots/capital_distribution/300episodes_full.png")
     plt = UnicodePlots.lineplot(1:NUM_EPISODES, total_rewards, xlabel="Episode", ylabel="total reward", title="DDPG Training", width=100)
     display(plt)
 end
