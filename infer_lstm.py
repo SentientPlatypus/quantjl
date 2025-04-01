9bd68fbcdb08f7769ecf901c3e76f8fa0e324e69
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2025-03-31 21:34:17 -0400

    added script

--- infer_lstm.py
+++ infer_lstm.py
@@ -51,36 +51,38 @@ def infer(ticker:str, model_filename:str = "msft_keras_model.keras", future_days
     scaler = MinMaxScaler(feature_range=(0,1))
 
     close_values = data.iloc[:, 4:5].values
-    print(data)
-    print(close_values)
 
-    past_100_days = close_values[-100:]
-    print(past_100_days)
+
+    past_100_days = close_values[-100:].reshape(-1, 1)  # Get the last 100 days of closing prices
+
     input_data = scaler.fit_transform(past_100_days)
     predictions = []
 
     for _ in range(future_days):
         next_day_scaled = model.predict(np.array([input_data]))
         next_day = scaler.inverse_transform(next_day_scaled)
-        predictions.append(next_day[0][0])
+        predictions.append(float(next_day[0][0]))
         input_data = np.append(input_data, next_day_scaled, axis=0)[-100:]
     return predictions, past_100_days
 
-def plot_movement(past, future, past_label="Past Prices", future_label="Predicted Prices"):
+def plot_movement(past, future, ticker):
+    plt.plot_size(100, 100)  # Set the size of the plot
 
     x_future = list(range(1, len(future) + 1)) 
     x_past = list(range(len(future) + 1, len(future) + len(past) + 1))
 
+    plt.plot(x_future, future, label="Predicted", color="blue")
+    plt.plot(x_past, past, label="Past", color="orange")
 
-    plt.plot(x_future, future, label=future_label, marker="dot", color="blue")
-    plt.plot(x_past, past, label=past_label, marker="dot", color="orange")
-
-    plt.title("Stock Price Prediction")
+    plt.title(f"{ticker} Stock Price Prediction")
     plt.xlabel("Days")
     plt.ylabel("Price")
+
     plt.grid(True)
-    
+    plt.theme("dark")  # Use a dark theme for the plot
+
     plt.show()
+
 
 
 if __name__ == "__main__":
@@ -90,8 +92,6 @@ if __name__ == "__main__":
     ticker = sys.argv[1]
 
     predictions, past_100 = infer(ticker, "AAPL_keras_model.keras", future_days=30)
-    
-    
-    plot_movement(past_100, predictions)
+    plot_movement(predictions, past_100.flatten().tolist(), ticker)
 
 
